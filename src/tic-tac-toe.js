39e27784d688e7588995e66d2812cb3b350bea06
Liza <lizayablonska@gmail.com>
2017-02-05 22:06:03 +0300

    2 test done

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,71 +1,81 @@
 class TicTacToe {
-    constructor() {
-        this.currentPlayerSymbol = "x";
-        this.matrix = new Array(3, 3);
 
+    constructor() {
+        this.currentPlayerSymbol = 'x';
+        this.winnerSymbol = null;
+        this.matrix = [[null,null,null],[null,null,null],[null,null,null]];
     }
 
     getCurrentPlayerSymbol() {
-    return this.currentPlayerSymbol;
+        return this.currentPlayerSymbol;
     }
 
-    nextTurn(rowIndex, columnIndex) {
-        // if(this.matrix[rowIndex][columnIndex]==null) {}
-        this.matrix[rowIndex][columnIndex] = this.currentPlayerSymbol;
-        if(this.currentPlayerSymbol = "x"){
-            this.currentPlayerSymbol = "o";
-        }else{
-            this.currentPlayerSymbol = "x";
+    nextTurn(rowIndex, colIndex) {
+
+        if(this.matrix[rowIndex][colIndex] == null) {
+            this.matrix[rowIndex][colIndex] = this.currentPlayerSymbol;
+
+            console.log(this.matrix[rowIndex][colIndex]);
+
+            if (this.currentPlayerSymbol == "x") {
+                console.log("2o");
+                this.currentPlayerSymbol = "o";
+            } else {
+                console.log("2x");
+                this.currentPlayerSymbol = "x";
+            }
+            this.isFinished();
         }
     }
 
     isFinished() {
-        // checks draw status
-        for(rowIndex=0; rowIndex<3; rowIndex++){
-            for(colIndex=0; colIndex<3; colIndex++){
-                if (matrix[rowIndex][colIndex]!=null){
-                    return true;
-                }
-            }
-        }
-
         // checks rows
-        for(rowIndex=0; rowIndex<3; rowIndex++){
-            if (matrix[rowIndex][0] == matrix[rowIndex][1]  == matrix[rowIndex][2]){
+        for(var rowIndex=0; rowIndex<3; rowIndex++){
+            if (this.matrix[rowIndex][0] == this.matrix[rowIndex][1]  == this.matrix[rowIndex][2]){
+                this.winnerSymbol = this.matrix[rowIndex][0];
                 return true;
             }
         }
-
         // check cols
-        for(colIndex=0; colIndex<3; colIndex++){
-            if (matrix[0][colIndex] == matrix[1][colIndex] == matrix[2][colIndex]){
+        for(var colIndex=0; colIndex<3; colIndex++){
+            if (this.matrix[0][colIndex] == this.matrix[1][colIndex] == this.matrix[2][colIndex]){
+                this.winnerSymbol = this.matrix[0][colIndex];
                 return true;
             }
         }
-        // chech diag
-        if ((matrix[0][0] == matrix[1][1] == matrix[2][2]) || (matrix[0][2] == matrix[1][1] == matrix[2][0])){
+        // check diags
+        if ((this.matrix[0][0] == this.matrix[1][1] == this.matrix[2][2])||(this.matrix[0][2] == this.matrix[1][1] == this.matrix[2][0])){
+            this.winnerSymbol = this.matrix[1][1];
            return true;
         }
-
         return false;
     }
 
-
-
     getWinner() {
-
+        return this.winnerSymbol;
     }
 
     noMoreTurns() {
-
+        for(var rowIndex=0; rowIndex<3; rowIndex++){
+            for(var colIndex=0; colIndex<3; colIndex++){
+                if (this.matrix[rowIndex][colIndex] == null){
+                    return false;
+                }
+            }
+        }
     }
 
     isDraw() {
-
+        if(this.noMoreTurns() && this.getWinner() == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     getFieldValue(rowIndex, colIndex) {
-    return rowIndex
+        return this.matrix[rowIndex][colIndex];
+
     }
 }
 
