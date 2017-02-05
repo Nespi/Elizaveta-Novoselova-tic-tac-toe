eead63e420615881e2eb2ac6e129b3ce5914dcae
Liza <lizayablonska@gmail.com>
2017-02-05 14:05:37 +0300

    arr

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,17 +1,37 @@
 class TicTacToe {
     constructor() {
+        this.currentPlayerSymbol = "x";
+        this.matrix = new Array(3, 3);
 
     }
 
     getCurrentPlayerSymbol() {
-
+    return this.currentPlayerSymbol;
     }
 
     nextTurn(rowIndex, columnIndex) {
-
+        // if(this.matrix[rowIndex][columnIndex]==null) {}
+        this.matrix[rowIndex][columnIndex] = this.currentPlayerSymbol;
+        if(this.currentPlayerSymbol = "x"){
+            this.currentPlayerSymbol = "o";
+        }else{
+            this.currentPlayerSymbol = "x";
+        }
     }
 
     isFinished() {
+
+        // checks rows
+        for(rowIndex=0; rowIndex<3; rowIndex++){
+            for(colIndex=0; colIndex<3; colIndex++) {
+                if(matrix[rowIndex][rowIndex] != 'x') {
+                    break;
+                } else {}
+
+            }
+        }
+        // check cols
+        // chech diag
 
     }
 
@@ -28,7 +48,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+    return rowIndex
     }
 }
 
